f362b3ecbd7845fd6f36eae2a0dd6a1d98f116e8
keerthisai <yenumula.keerthisai013@gmail.com>
2019-12-15 22:47:13 -0500

    * learnings.csproj: removed ununeccesary libraries
    * Program.cs:

---
+++ learnings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace learnings
+{
+    class MainClass
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter text");
+            string fullName = Console.ReadLine(); // "keerthi sai yanumula";
+            string[] fname = fullName.Split(' ');
+            string nameconverted = "";
+            StringBuilder sb = new StringBuilder();
+            foreach(var name in fname)
+            {
+                if(name.Length > 2)
+                {
+                    string distinct = new string(name.Substring(1, name.Length - 2).Distinct().ToArray());
+                    nameconverted = $"{name[0]}{distinct.Length}{name[name.Length - 1]}";
+                }
+                else
+                {
+                    nameconverted = name;
+                }
+                sb.Append(nameconverted + " ");
+            }
+            string result = sb.ToString();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+       
+
+
+    }
+}
